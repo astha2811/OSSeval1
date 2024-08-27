a44280c60ce88ce77d5033286fdcb7226efc3714
astha2811 <144266899+astha2811@users.noreply.github.com>
2024-08-27 16:11:42 +0530

    q2.py

---
+++ q2.py
@@ -0,0 +1,25 @@
+sentence = input(str("enter any sentence"))
+def countalpha(sentence)
+    alpha_freq ={}
+    alpha = char.upper() for char in sentence
+    for x in alpha
+        if x in alpha_freq:
+            alpha_freq[w] +=1
+        else:
+            alpha_freq[w]=1
+    return alpha_freq
+
+def countalpha(sentence)
+    word_freq ={}
+    word = char.upper() for char in sentence
+    for x in word
+        if x in word_freq:
+            word_freq[w] +=1
+        else:
+            word_freq[w]=1
+  return word_freq
+
+
+
+    
+        
